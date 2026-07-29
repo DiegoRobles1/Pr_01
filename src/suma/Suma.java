c65f1ba5388de94f4587e610368291a2a972938a
Diego Robles <2610452@cesba-queretaro.edu.mx>
2026-07-28 23:28:39 -0600

    Este es mi primer commit

---
+++ src/suma/Suma.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma;
+
+/**
+ *
+ * @author diego
+ */
+public class Suma {
+
+    public static void main(String[] args) {
+        int x=2;
+        int y=23;
+        int suma=x+y;
+        System.out.println("suma:"+suma);
+    }
+    
+}
